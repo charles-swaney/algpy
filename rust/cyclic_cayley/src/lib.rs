d3a79557ab578d3879c3314292d8bf6fd7f1ec54
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-04-07 20:20:59 -0400

    update cyclic group table generation

--- rust/cyclic_cayley/src/lib.rs
+++ rust/cyclic_cayley/src/lib.rs
@@ -1,20 +1,18 @@
 use pyo3::prelude::*;
 use pyo3::Python;
-use pyo3::types::{PyDict, PyList};
+use pyo3::types::PyDict;
 
 #[pyfunction]
 fn gen_cyclic_table(py: Python, n_elements: usize) -> PyResult<PyObject> {
     let table = PyDict::new(py);
-    
     for i in 0..n_elements {
-        let mut row = Vec::new();
+        let row = PyDict::new(py);
         for j in 0..n_elements {
-            row.push((j + i) % n_elements);
+            let value = ((i + j) % n_elements).to_object(py);
+            row.set_item(j, &value)?;
         }
-        let py_row: PyObject = PyList::new(py, &row).to_object(py);
-        table.set_item(i.to_object(py), py_row)?;
+        table.set_item(i.to_object(py), row)?;
     }
-
     Ok(table.into())
 }
 
@@ -23,6 +21,4 @@ fn gen_cyclic_cayley(_py: Python, m: &PyModule) -> PyResult<()> {
     // Corresponding Python module.
     m.add_function(wrap_pyfunction!(gen_cyclic_table, m)?)?;
     Ok(())
-}
-
-
+} 
